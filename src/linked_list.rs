fbc6956aa582142d0529a99ea0d8880f0b9c825b
Darius-Andrei Jipa <jipad16@yahoo.com>
2023-08-10 22:25:51 +0300

    Made stack-like linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,13 +1,33 @@
-#[derive(Debug, PartialEq)]
-struct LinkedList {
+pub struct LinkedList {
+    head: Option<Box<Node>>,
+}
+
+struct Node {
     data: u32,
-    next: Option<Box<LinkedList>>,
+    next: Option<Box<Node>>,
 }
 
 #[allow(dead_code)]
 impl LinkedList {
-    fn new(data: u32) -> LinkedList {
-        LinkedList { data, next: None }
+    fn new() -> LinkedList {
+        LinkedList { head: None }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    fn push(&mut self, data: u32) {
+        let new_head = Box::new(Node{ data, next: self.head.take()});
+        self.head = Some(new_head);
+    }
+
+    fn pop(&mut self) -> Option<u32> {
+        self.head.take().map(|n| {self.head = n.next; n.data})
+    }
+
+    fn peek(&self) -> Option<&u32> {
+        self.head.as_ref().map(|n| {&n.data})
     }
 }
 
@@ -17,8 +37,20 @@ mod tests {
 
     #[test]
     fn test_create() {
-        let list: LinkedList = LinkedList::new(23);
-        assert_eq!(list.data, 23);
-        assert_eq!(list.next, None);
+        let list = LinkedList::new();
+        assert!(list.is_empty());
+    }
+    
+    #[test]
+    fn test_list() {
+        let mut list = LinkedList::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        assert_eq!(list.pop().unwrap(), 4);
+        assert_eq!(list.pop().unwrap(), 3);
+        assert_eq!(list.pop().unwrap(), 2);
+        assert_eq!(list.pop().unwrap(), 1);
     }
 }
